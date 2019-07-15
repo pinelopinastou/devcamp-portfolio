8a0e50ada6511a4287f9ec6f1c96b76213f1d87a
penelopenastou <penelope@workable.com>
2019-07-15 19:51:13 +0300

    Added nav bar and default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+
+	included do
+	 before_filter :set_page_defaults
+	end
+
+	def set_page_defaults
+	  @page_title = "My Portfolio"
+	  @seo_keywords = "Penelope Nastou Portfolio"
+	end
+end
